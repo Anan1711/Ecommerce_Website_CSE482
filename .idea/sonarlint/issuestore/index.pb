88bf8a4a3ca6e8a110c2330fbba3d37c0d97e69d
MdAzizulHossain <azizulhossainnoble@gmail.com>
2020-12-10 17:35:33 +0600

    Site Functionality
    Add to Cart, Update Cart,Shipping Address, Checking Form, Process Order, Token problem fixing

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+O
+store/templates/store/main.html,3\2\32cb3af21a6ca24a7c00564c22570ef5c85534b0
+P
+ store/templates/store/store.html,4\a\4a71b91d0923bc92b3012242d47c9585f185080f
+>
+store/views.py,c\8\c842493f8c3c6ac29ef61cab9fc46d3af02e60bc
+=
+store/urls.py,9\a\9a14031b5a4a451338435fa5c54f7b76fe1b4808
+A
+static/js/cart.js,0\6\0663754275559e707f0342e424ef60a8970a9d84
--- store/models.py
+++ store/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200)
+    email = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
@@ -15,7 +15,7 @@ class Customer(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=7, decimal_places=2)
-    digital = models.BooleanField(default=False,null=True, blank=True)
+    digital = models.BooleanField(default=False,null=True, blank=False)
     image = models.ImageField(null=True, blank=True) 
 
     def __str__(self):
@@ -32,11 +32,20 @@ class Product(models.Model):
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
-    complete = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False, null=True, blank=False)
     transaction_id = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return str(self.id)
+
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+        return shipping
 
     @property
     def get_cart_total(self):
